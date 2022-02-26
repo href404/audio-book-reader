e7a4357dd9cb3ed57217927e9980edffb0f84781
Pierre Julien <pierrejulienpro@gmail.com>
2022-02-26 20:37:24 +0100

    Ajout et renommage de méthodes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import Tesseract from "tesseract.js";
 import "./style.css";
 
-const video = document.querySelector("video");
-const text = document.querySelector("[data-text]");
+const video = document.querySelector("video") as HTMLVideoElement;
+const text = document.querySelector("[data-text]") as HTMLPreElement;
 
 async function init() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-    if (!video) return;
     video.srcObject = stream;
     video.addEventListener("playing", async () => onVideoPlaying());
 }
@@ -18,38 +16,44 @@ async function onVideoPlaying() {
 
 async function onKeyPress(e: KeyboardEvent) {
     if (e.code !== "Space") return;
-    await detectTextFromCamera();
-}
 
-async function detectTextFromCamera() {
-    const worker = await getTesseractWorker();
-    const canvas = document.createElement("canvas");
+    const data = await getTextFromVideo();
 
-    if (!video) return;
-    canvas.width = video.width;
-    canvas.height = video.height;
-
-    const twoDimensionContext = canvas.getContext("2d");
-    if (!twoDimensionContext) return;
-
-    twoDimensionContext.drawImage(video, 0, 0, video.width, video.height);
-
-    const result = await worker.recognize(canvas);
-    const data = result.data.text;
-    const utterance = new SpeechSynthesisUtterance(data.replace(/\s/g, " "));
-
-    speechSynthesis.speak(utterance);
-
-    if (!text) return;
+    speak(data);
     text.textContent = data;
 }
 
+async function getTextFromVideo() {
+    const worker = await getTesseractWorker();
+    const image = getImageFromVideo();
+
+    const result = await worker.recognize(image);
+    return result.data.text;
+}
+
 async function getTesseractWorker() {
+    const language = "eng";
     const worker = Tesseract.createWorker();
     await worker.load();
-    await worker.loadLanguage("eng");
-    await worker.initialize("eng");
+    await worker.loadLanguage(language);
+    await worker.initialize(language);
     return worker;
+}
+
+function getImageFromVideo() {
+    const canvas = document.createElement("canvas");
+    canvas.width = video.width;
+    canvas.height = video.height;
+
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    context.drawImage(video, 0, 0, video.width, video.height);
+
+    return canvas;
+}
+
+function speak(data: string) {
+    const utterance = new SpeechSynthesisUtterance(data.replace(/\s/g, " "));
+    speechSynthesis.speak(utterance);
 }
 
 init();
